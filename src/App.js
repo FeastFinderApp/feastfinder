2bc80ec5ab671eb3076501e5e9a8922b810c1ea0
Maruko Aizawa <80887472+Mark-Barican@users.noreply.github.com>
2024-09-22 19:50:33 +0800

    Fixes and Placeholders
    -Added color to active pages for links in nav bar
    -placeholder added for log in and admin pages

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./components/Home";
-import About from "./components/About";
-import SampleResto from "./components/SampleResto";
-import Login from "./components/Login";
-import LoginAdmin from "./components/LoginAdmin";
-import "./components/Home.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+import About from './components/About';
+import SampleResto from './components/SampleResto';
+import Login from './components/Login';
+import LoginAdmin from './components/LoginAdmin'; // Ensure this path is correct
+import './components/Home.css';
 
 function App() {
   return (
     <Router>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand as={Link} to="/">
-            feastfinder
-          </Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">feastfinder</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {" "}
-              {/*Will push links to the right */}
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about">
-                About
-              </Nav.Link>
-              <Nav.Link as={Link} to="/sample">
-                Sample
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/admin">
-                Admin
-              </Nav.Link>
+              <Nav.Link as={NavLink} to="/" className={({ isActive }) => isActive ? 'active' : ''}>Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/about" className={({ isActive }) => isActive ? 'active' : ''}>About</Nav.Link>
+              <Nav.Link as={NavLink} to="/sample" className={({ isActive }) => isActive ? 'active' : ''}>Sample</Nav.Link>
+              <Nav.Link as={NavLink} to="/login" className={({ isActive }) => isActive ? 'active' : ''}>Login</Nav.Link>
+              <Nav.Link as={NavLink} to="/admin" className={({ isActive }) => isActive ? 'active' : ''}>Admin</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
