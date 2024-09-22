85a74baeda828908f39fd13f4ac8ff347ba9d02c
NoelleAlt <noellesf09@gmail.com>
2024-09-22 21:46:43 +0800

    typo fix

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <h2>About Feastfinder</h2>
         <p>
           FeastFinder connects you with local restaurants and ensures quick,
-          reliable delivery to your doorstep. With our plaform,
+          reliable delivery to your doorstep. With our platform,
           we aim to ensure that you can create a website for your restaurant
           without relying on the limits imposed by other food delivery apps.
         </p>
