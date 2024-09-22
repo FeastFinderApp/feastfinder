3530ec6d277dc30b0ce32d04ce568c1f95e63924
Maruko Aizawa <80887472+Mark-Barican@users.noreply.github.com>
2024-09-22 21:10:13 +0800

    changes didn't go through "fixed"
    -previous changes didn't go through
    
    +animation
    +styling
    +nav bar changes
    +nav bar links fixed
    +404 page working

--- src/components/404NotFound.js
+++ src/components/404NotFound.js
@@ -1,4 +1,3 @@
-// src/components/404NotFound.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './404NotFound.css'; // Optional for custom styles
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./LoginForm.css"; // For custom styles, if needed
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
     <div className="login-wrapper">
-      <form action="">
-        <h1>Login</h1>
+      <form>
+        <h1 className="login-text">Login</h1>
         <div className="input-box">
           <input type="text" placeholder="Username" required />
+          <FaUser className="icon"/>
         </div>
         <div className="input-box">
           <input type="password" placeholder="Password" required />
+          <FaLock className="icon"/>
         </div>
 
         <div className="remember-forgot">
@@ -18,14 +23,14 @@ const Login = () => {
             <input type="checkbox" />
             Remember me
           </label>
-          <a href="#">Forgot Password?</a>
+          <NavLink to="/forgot-password">Forgot Password?</NavLink>
         </div>
 
         <button type="submit">Login</button>
 
         <div className="register-link">
           <p>
-            Don't have an account? <a href="#">Register</a>
+            Don't have an account? <NavLink to="/register">Register</NavLink>
           </p>
         </div>
       </form>
