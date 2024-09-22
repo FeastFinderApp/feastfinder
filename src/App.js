8c6f1ec8f7e5c7fe0facdd0d9bb14dcfe221f002
Matthew Marcelo <git@matthewmarcelo.com>
2024-09-22 16:14:35 +0800

    create router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home'; // Import the Home component
 import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Home />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+
+        </Routes>
       {/* Other components like Footer can go here */}
-    </div>
+      </Router>
+      
   );
 }
 
