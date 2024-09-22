7673305c4f856185d765e9d994027ca017fa4b16
Maruko Aizawa <80887472+Mark-Barican@users.noreply.github.com>
2024-09-22 19:27:44 +0800

    Animation for Home and Formatting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home'; // Import the Home component
-import Navigation from './components/Navigation';
-import About from './components/About';
-import SampleResto from './components/SampleResto';
-import Login from './components/Login';
-import LoginAdmin from './components/LoginAdmin';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import SampleResto from "./components/SampleResto";
+import Login from "./components/Login";
+import LoginAdmin from "./components/LoginAdmin";
+import "./components/Home.css";
 
 function App() {
   return (
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/sample" element={<SampleResto />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<LoginAdmin />} />
-        </Routes>
-      {/* Other components like Footer can go here */}
-      </Router>
-      
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            feastfinder
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {" "}
+              {/*Will push links to the right */}
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/sample">
+                Sample
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/admin">
+                Admin
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sample" element={<SampleResto />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<LoginAdmin />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/SampleResto.js
+++ src/components/SampleResto.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-function SampleResto(){
-    return(
-        <div>
-            
-        </div>
-    );
+function SampleResto() {
+  return <div></div>;
 }
 
 export default SampleResto;
