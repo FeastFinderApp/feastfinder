b9ca325a81a2915397cde38246bfaa0b69cd9563
Matthew Marcelo <git@matthewmarcelo.com>
2024-09-22 17:31:31 +0800

    add navigation links, fix #5

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
-function Nav(){
+function Navigation(){
     return(
-      <Navbar className="navbar">
+        <Navbar className="navbar">
         <Container>
             <Navbar.Brand href="#home" className='highlight_text' style={{ fontSize: '2rem' }}>Feast Finder</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="about">About</Nav.Link>
+                <Nav.Link as={Link} to="sample">Sample</Nav.Link>
+                <Nav.Link as={Link} to="login">Login</Nav.Link>
+                <Nav.Link as={Link} to="admin">Admin</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
         </Container>
         </Navbar>
     );
 }
 
-export default Nav;
+export default Navigation;
