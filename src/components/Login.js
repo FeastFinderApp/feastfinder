a2cf1c6f47ef4c42fe714e32c2de706726e52a50
Maruko Aizawa <80887472+Mark-Barican@users.noreply.github.com>
2024-09-22 20:43:40 +0800

    404 and login page added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./LoginForm.css"; // For custom styles, if needed
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
     <div className="login-wrapper">
       <form>
-        <h1>Login</h1>
+        <h1 className="login-text">Login</h1>
         <div className="input-box">
           <input type="text" placeholder="Username" required />
+          <FaUser className="icon"/>
         </div>
         <div className="input-box">
           <input type="password" placeholder="Password" required />
+          <FaLock className="icon"/>
         </div>
 
         <div className="remember-forgot">
