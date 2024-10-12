087bdbead0884a09b141c0b68dce8128f8bfad3a
Maruko Aizawa <80887472+Mark-Barican@users.noreply.github.com>
2024-10-12 22:34:52 +0800

    Fixed FAQ
    -for some reason FAQ page was causing an error because of improper nesting structure "ul" and "h2" tags

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,14 +6,13 @@ const FAQ = () => {
     <div>
       {/* FAQ Page */}
       <section id="FAQ" className="FAQ">
-        <h1>FAQs :</h1>
-        <ul><h2>What makes your services unique from
-            other similar platforms?</ul></h2>
-            
-        <ul> <p>Compared to similar platforms, there's less requirements</p>
-          <p> needed to use our services for menu making. Other platforms</p> 
-          <p>require a subscription or partnership, and we aim to not</p>
-          <p> hassle restaurant owners with requirements such as those</p> </ul>
+        <h1>FAQs:</h1>
+        <div>
+          <h2>What makes your services unique from other similar platforms?</h2>
+          <p>
+            Compared to similar platforms, there's less requirements needed to use our services for menu making. Other platforms require a subscription or partnership, and we aim to not hassle restaurant owners with requirements such as those.
+          </p>
+        </div>
       </section>
     </div>
   );
