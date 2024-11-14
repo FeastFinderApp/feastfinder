ca7cc14a77bba947f8da2a3786215a893875a9ea
Matthew Marcelo <git@matthewmarcelo.com>
2024-11-14 16:15:59 +0800

    change class to className

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios";
 
 function CartItem(props) {
     const [count, setCount] = useState(0);
@@ -8,14 +7,14 @@ function CartItem(props) {
     }, []);
 
     return(
-        <div class="cartItem">
-            <div class="cartDescription">
-                <div class="cartItemName">{props.name}</div>
-                <div class="cartPrice">{props.price}</div>
+        <div className="cartItem">
+            <div className="cartDescription">
+                <div className="cartItemName">{props.name}</div>
+                <div className="cartPrice">{props.price}</div>
             </div>
-            <div class="qty">
+            <div className="qty">
                 <button onClick={() => setCount(prev => prev - 1)}>-</button>
-                <div class="qtyCount">{count}</div>
+                <div className="qtyCount">{count}</div>
                 <button onClick={() => setCount(prev => prev + 1)}>+</button>
             </div>
         </div>
